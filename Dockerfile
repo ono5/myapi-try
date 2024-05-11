9166bfbab29c5566510f215110a8a5d294cc6629
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-05-12 06:52:06 +0900

    add: hotreload by air

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,4 @@
 FROM golang:latest AS builder
 
 WORKDIR /app
-
-COPY go.mod . 
-COPY go.sum .
-
-RUN go mod download
-
-COPY . .
-
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app ./cmd/api
-
-FROM alpine:latest  
-
-RUN apk --no-cache add ca-certificates
-
-WORKDIR /root/
-
-COPY --from=builder /app/app .
-
-CMD ["./app"]
+RUN go install github.com/cosmtrek/air@latest
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,13 +8,13 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Worldi!"))
 }
 
 func main() {
 	router := chi.NewRouter()
-	router.Get("/", handler)
+	router.Get("/", homeHandler)
 	fmt.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
