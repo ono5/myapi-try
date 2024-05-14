2a783b1b2e39c35de397a2cf4fae13e2ca8735e6
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-05-15 08:05:26 +0900

    handlerの実装(途中)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Worldi!"))
 }
 
+type application struct{}
+
 func main() {
-	router := chi.NewRouter()
-	router.Get("/", homeHandler)
-	fmt.Println("Server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	app := &application{}
+	app.routes()
 }
